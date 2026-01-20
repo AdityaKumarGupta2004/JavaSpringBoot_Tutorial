49e7615d535cc6c0b0ea396768c74b9a7edd44d4
Aditya Kumar Gupta <adityakumargupta45321@gmail.com>
2026-01-20 15:21:42 +0530

    3rd commit

---
+++ learningRestApi/src/main/java/com/learningjava/learningRestApi/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.learningjava.learningRestApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping
+
+}
